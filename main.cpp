b6f392871346f16d4ef59f9445247acc7c4f7547
Aref <Aref.Najmaddini@gmail.com>
2023-01-07 20:08:57 +0330

    fix main

--- main.cpp
+++ main.cpp
@@ -6,20 +6,7 @@
 int main()
 {
     Timer timer;
-
-    std::string image_path = interpolation("data\\mnist", "train", "7");
-    float img[IMAGE_SIZE][IMAGE_SIZE];
-
-    load_image(image_path, 100, img);
-    // print matrix image
-    for (int i = 0; i < IMAGE_SIZE; i++)
-    {
-        for (int j = 0; j < IMAGE_SIZE; j++)
-        {
-            std::cout << img[i][j] << "   ";
-        }
-        std::cout << std::endl;
-    }
+    start_menu();
 
     return 0;
 }
